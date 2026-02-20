4467960eb4a8a61b696d8468965595dbb1d432f1
Emauri Neto <emaurigomesneto@gmail.com>
2026-02-20 18:35:34 -0300

    feat: add fonts and default styles

---
+++ client/src/components/fonts/index.ts
@@ -0,0 +1,13 @@
+// Space Grotesk
+import '@fontsource/space-grotesk/400.css'
+import '@fontsource/space-grotesk/600.css'
+import '@fontsource/space-grotesk/700.css'
+
+// Source Serif 4
+import '@fontsource/source-serif-4/400.css'
+import '@fontsource/source-serif-4/600.css'
+import '@fontsource/source-serif-4/700.css'
+
+// Source Code Pro
+import '@fontsource/source-code-pro/400.css'
+import '@fontsource/source-code-pro/600.css'
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -5,6 +5,12 @@ export const Route = createFileRoute('/')({ component: App })
 function App() {
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900"></div>
+    <div className="dark min-h-screen bg-background w-full">
+      <main className='mx-auto w-full max-w-7xl px-2.5 md:px-20l'>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl text-primary">
+          Hello World
+        </div>
+      </main>
+    </div>
   )
 }
